51a97182aea697790c2a6720eab4b89263a12891
koolkat <flores_a@epitech.eu>
2016-01-12 16:25:02 +0100

    added jsx support and eslint checking

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -1,13 +1,13 @@
 ;; javascript / html
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-(add-hook 'js-mode-hook 'subword-mode)
-(add-hook 'html-mode-hook 'subword-mode)
-(setq js-indent-level 2)
-(eval-after-load "sgml-mode"
-  '(progn
-     (require 'tagedit)
-     (tagedit-add-paredit-like-keybindings)
-     (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
+;; (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+;; (add-hook 'js-mode-hook 'subword-mode)
+;; (add-hook 'html-mode-hook 'subword-mode)
+;; (setq js-indent-level 2)
+;; (eval-after-load "sgml-mode"
+;;   '(progn
+;;      (require 'tagedit)
+;;      (tagedit-add-paredit-like-keybindings)
+;;      (add-hook 'html-mode-hook (lambda () (tagedit-mode 1)))))
 
 
 ;; coffeescript
@@ -27,18 +27,36 @@
 (setq web-mode-content-types-alist
   '(("jsx" . "\\.js[x]?\\'")))
 
-(require 'flycheck)
-(flycheck-define-checker jsxhint-checker
-  "A JSX syntax and style checker based on JSXHint."
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+    (let ((web-mode-enable-part-face nil))
+      ad-do-it)
+    ad-do-it))
 
-  :command ("jsxhint" source)
-  :error-patterns
-  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
-  :modes (web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (equal web-mode-content-type "jsx")
-              ;; enable flycheck
-              (flycheck-select-checker 'jsxhint-checker)
-              (flycheck-mode))))
-(require 'web-mode)
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+
+;; turn on flychecking globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(json-jsonlist)))
+
+;; https://github.com/purcell/exec-path-from-shell
+;; only need exec-path-from-shell on OSX
+;; this hopefully sets up path and other vars better
+;; (when (memq window-system '(mac ns))
+;;   (exec-path-from-shell-initialize))
---
+++ install/install.sh
@@ -0,0 +1,3 @@
+#http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+
+npm install -g eslint babel-eslint eslint-plugin-react
