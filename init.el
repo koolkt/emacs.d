6bbb0ee187a7fccb232c49f22a8539bedfc902d7
koolkat <flores_a@epitech.eu>
2016-01-14 23:38:46 +0100

    added dependencies

--- init.el
+++ init.el
@@ -77,8 +77,10 @@
     tagedit
 
     ;; git integration
+    web-mode
     magit
-    
+    clj-refactor
+    git-gutter
     flycheck
     php-auto-yasnippets
     ac-emmet
@@ -108,7 +110,7 @@
 ;;
 ;; (require 'yaml-mode)
 ;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-;; 
+;;
 ;; Adding this code will make Emacs enter yaml mode whenever you open
 ;; a .yml file
 (add-to-list 'load-path "~/.emacs.d/vendor")
